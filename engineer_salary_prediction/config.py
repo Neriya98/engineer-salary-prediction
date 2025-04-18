5a62b56b6ff63c4675fd911d8a9c042311039d8c
Neriya98 <baruchdakpovi@gmail.com>
2025-04-18 19:49:40 +0100

    Creation of the notebook

--- engineer_salary_prediction/config.py
+++ engineer_salary_prediction/config.py
@@ -1,16 +1,17 @@
 from pathlib import Path
 
-from dotenv import load_dotenv
+#from dotenv import load_dotenv
 from loguru import logger
 
 # Load environment variables from .env file if it exists
-load_dotenv()
+#load_dotenv()
 
 # Paths
 PROJ_ROOT = Path(__file__).resolve().parents[1]
 logger.info(f"PROJ_ROOT path is: {PROJ_ROOT}")
 
 DATA_DIR = PROJ_ROOT / "data"
+SUBMISSIONs_DIR = DATA_DIR / "submissions"
 RAW_DATA_DIR = DATA_DIR / "raw"
 INTERIM_DATA_DIR = DATA_DIR / "interim"
 PROCESSED_DATA_DIR = DATA_DIR / "processed"
--- engineer_salary_prediction/dataset.py
+++ engineer_salary_prediction/dataset.py
@@ -12,18 +12,19 @@ app = typer.Typer()
 @app.command()
 def main(
     # ---- REPLACE DEFAULT PATHS AS APPROPRIATE ----
-    input_path: Path = RAW_DATA_DIR / "dataset.csv",
-    output_path: Path = PROCESSED_DATA_DIR / "dataset.csv",
+    train_path: Path = RAW_DATA_DIR / "train.csv",
+    test_path: Path = PROCESSED_DATA_DIR / "test.csv",
     # ----------------------------------------------
 ):
     # ---- REPLACE THIS WITH YOUR OWN CODE ----
     logger.info("Processing dataset...")
+    
     for i in tqdm(range(10), total=10):
         if i == 5:
             logger.info("Something happened for iteration 5.")
     logger.success("Processing dataset complete.")
     # -----------------------------------------
-
+    
 
 if __name__ == "__main__":
     app()
